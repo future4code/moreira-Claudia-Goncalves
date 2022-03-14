85858455a10f930e3c50842f17145f0c104bbbe0
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-03-14 11:16:47 -0300

    implementações realizadas

--- modulo3/PRI/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo3/PRI/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    nomeUsuario: '',
+    inputMensagem: '',
+    mensagens: [],
+  }
+
+  onChangeUsuario = (e) => {
+    this.setState({ nomeUsuario: e.target.value })
+  }
+
+  onChangeInputMensagem = (e) => {
+    this.setState({ inputMensagem: e.target.value })
+  }
+
+  enviarMensagem = () => {
+    const mensagemEnviada = {
+      usuario: this.state.nomeUsuario,
+      textoMensagem: this.state.inputMensagem
+    }
+
+    const mensagemEnviadaFormatada = [...this.state.mensagens, mensagemEnviada]
+
+    this.setState({ mensagens: mensagemEnviadaFormatada })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Whats Lab</h1>
+
+        {this.state.mensagens.map((item, index) => {
+          return (
+            <p key={index}>
+              <b>{item.usuario}</b>: {item.textoMensagem}
+            </p>
+          )
+        })}
+
+        <input
+          placeholder="Seu nome"
+          onChange={this.onChangeUsuario}
+          value={this.state.nomeUsuario} />
+
+        <input
+          placeholder="Digite sua mensagem"
+          onChange={this.onChangeInputMensagem}
+          value={this.state.inputMensagem} />
+
+        <button onClick={this.enviarMensagem}>Enviar</button>
+
+      </div>
+    )
+  }
 }
 
 export default App;
