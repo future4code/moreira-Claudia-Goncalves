07b670e9857fac4ebbfbdeb84470e077055158e6
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-03-16 11:15:31 -0300

    concluido

--- modulo3/PRI/filtros-e-ordenacao/filtro-e-ordenacao/src/App.js
+++ modulo3/PRI/filtros-e-ordenacao/filtro-e-ordenacao/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import jobList from "./data/jobs.json"
+import { Header } from "./components/Header";
+import { Card } from "./components/Card";
+import { ListContainer } from "./style";
+import { Filtros } from "./components/Filtros";
+export class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   state = {
+      jobs: jobList,
+      busca: "",
+      precoMinimo: "",
+      precoMaximo: "",
+      parametroOrdenacao: "title",
+      order: 1
+   }
+
+   onChangeAtualizarPesquisa = (e) => {
+      this.setState({ busca: e.target.value })
+   }
+
+   onChangePrecoMinimo = (e) => {
+      this.setState({ precoMinimo: e.target.value })
+   }
+
+   onChangePrecoMaximo = (e) => {
+      this.setState({ precoMaximo: e.target.value })
+   }
+
+   onChangeParametroOrdenacao = (e) => {
+      this.setState({ parametroOrdenacao: e.target.value })
+   }
+
+   onChangeOrder = (e) => {
+      this.setState({ order: e.target.value })
+   }
+
+   render() {
+      return <>
+         <Header />
+         <Filtros
+            busca={this.state.busca}
+            precoMinimo={this.state.precoMinimo}
+            precoMaximo={this.state.precoMaximo}
+            parametroOrdenacao={this.state.parametroOrdenacao}
+            order={this.state.order}
+            onChangeAtualizarPesquisa={this.onChangeAtualizarPesquisa}
+            onChangePrecoMinimo={this.onChangePrecoMinimo}
+            onChangePrecoMaximo={this.onChangePrecoMaximo}
+            onChangeParametroOrdenacao={this.onChangeParametroOrdenacao}
+            onChangeOrder={this.onChangeOrder}
+         />
+         <ListContainer>
+            {this.state.jobs
+               .filter(job => { //texto campo de busca
+                  return (
+                     job.title.toLowerCase().includes(this.state.busca.toLowerCase()) || job.description.toLowerCase().includes(this.state.busca.toLowerCase())
+                  )
+               })
+               .filter(job => {
+                  return (
+                     this.state.precoMinimo === "" || job.price >= this.state.precoMinimo
+                  )
+               })
+               .filter(job => {
+                  return (
+                     this.state.precoMaximo === "" || job.price <= this.state.precoMaximo
+                  )
+               })
+               .sort((atualJob, proximoJob) => {
+                  switch (this.state.parametroOrdenacao) {
+                     case "title":
+                        return this.state.order * atualJob.title.localeCompare(proximoJob.title)
+                     case "dueDate":
+                        return this.state.order * (new Date(atualJob.dueDate).getTime() - new Date(proximoJob.dueDate).getTime())
+                     default:
+                        return this.state.order * (atualJob.price - proximoJob.price)
+                  }
+               })
+               .map(job => {
+                  return <Card key={job.id} job={job} />
+               })}
+         </ListContainer>
+      </>
+   }
 }
-
-export default App;
---
+++ modulo3/PRI/filtros-e-ordenacao/filtro-e-ordenacao/src/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+   display: flex;
+   justify-content: space-between;
+   flex-wrap:wrap;
+   *{
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+   }
+`
+
+export const FilterContainer = styled.div`
+   display: flex;
+   justify-content: space-between;
+`
