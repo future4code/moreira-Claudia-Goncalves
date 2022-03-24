0ae425e8f31c4eda0fda6079e914be296e11149a
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-03-24 08:46:53 -0300

    exercicio2 (com erro)

--- modulo4/intro-typescript/src/exercicio2.ts
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -1,10 +1,34 @@
-const cor1: string = prompt("informe uma cor:")
+const readline = require('readline');
+let cor1 = "";
+let cor2 = "";
+let cor3 = "";
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
-//   const arrayDeCores = []
+rl.question('Informe a primeira cor:', function (resposta1: string): any {
+  let cor1: string = resposta1;
+  rl.close();
+});
 
-//   arrayDeCores.push(cor1,cor2,cor3) 
-  
-//   return arrayDeCores
-// }
+rl.question('Informe a segunda cor:', function (resposta2: string): any {
+  let cor2: string = resposta2;
+  rl.close();
+});
+
+rl.question('Informe a terceira cor:', function (resposta3: string): any {
+  let cor3: string = resposta3;
+  rl.close();
+});
+
+function imprimeTresCoresFavoritas(resposta1: string, resposta2: string, resposta3: string): any {
+  const arrayDeCores: Array<string> = []
+
+  arrayDeCores.push(cor1, cor2, cor3)
+
+  return (arrayDeCores)
+}
+
+console.log(imprimeTresCoresFavoritas(cor1, cor2, cor3))
