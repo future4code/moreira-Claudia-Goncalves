81b4ead654ee632173073a247ff422b5d9aedf38
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-03-24 08:11:39 -0300

    exercicio1

---
+++ modulo4/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,10 @@
+const cor1: string = prompt("informe uma cor:")
+
+
+// function imprimeTresCoresFavoritas(cor1, cor2, cor3) {
+//   const arrayDeCores = []
+
+//   arrayDeCores.push(cor1,cor2,cor3) 
+  
+//   return arrayDeCores
+// }
