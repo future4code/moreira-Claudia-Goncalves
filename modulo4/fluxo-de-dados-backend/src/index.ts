1646a6419c5038353d717d0ab75181d291646ed2
Claudia Carvalho <60494846+claudia-carvalho@users.noreply.github.com>
2022-04-01 08:53:59 -0300

    update

--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -1,17 +1,43 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
+import { produtos } from './data';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.send("Pong! 🏓")
+//teste api
+app.get("/test", (req: Request, res: Response) => {
+  res.send("A api está funcionando!!!")
 })
 
+//criar novo produto
+app.post("/produtos", (req: Request, res: Response) => {
+  const nomeProduto = req.body.name
+  const precoProduto = req.body.price
 
+  type Produto = {
+    id: string,
+    name: string,
+    price: number
+  }
+
+  const novoProduto: Produto = {
+    id: Date.now().toString(),
+    name: nomeProduto,
+    price: precoProduto
+  }
+
+  // for (let i = 0; i < produtos.length; i++) {
+  //   produtos.push(novoProduto)
+  // }
+
+  // [...produtos, novoProduto]
+
+  res.send(produtos)
+})
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
